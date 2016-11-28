21bdfea593b8fac095abab9a0fa7860f7073d085
syney shen <syney.shen@outlook.com>
2016-11-28 16:24:05 +0800

    new feature.

--- main.go
+++ main.go
@@ -4,42 +4,92 @@ import (
 	"os"
 	"fmt"
 	"golang.org/x/crypto/ssh"
+	"strings"
 )
 
 const (
-	cParamsNum = 7 // include the executable file, user name, password, ip addr1, volume path on ip addr1, ip addr2, volume path on ip addr2.
+	//cParamsNum = 7 // include the executable file, user name, password, ip addr1, volume path on ip addr1, ip addr2, volume path on ip addr2.
 	cSSHPort = ":22"
+	cUserName = "root"
 )
 
 func main() {
-	if len(os.Args) != cParamsNum {
-		fmt.Println("Parameters are invalid. Please input like DeleteGFSVolume.exe user-name password ip1 volume-path ip2 volume-path.")
+	if len(os.Args) <= 2 {
+		fmt.Println("Parameters are invalid. Please input like DeleteGFSVolume.exe password 10.8.65.84:/home/brick1 10.8.65.84:/home/brick2 10.8.65.89:/home/brick3 10.8.65.89:/home/brick4 ...")
 		os.Exit(-1)
 	}
 
-	var host1 = Client{
-		userName: os.Args[1],
-		password: os.Args[2],
-		ipAddr: os.Args[3] + cSSHPort,
-		volume: os.Args[4],
+	pw := os.Args[1]
+
+	var hostsAndPaths = make(map[string][]string)
+
+	for i := 2; i < len(os.Args); i++ {
+		var flag bool = false
+		hostAndPath := strings.Split(os.Args[i], ":")
+
+		//fmt.Println(hostAndPath[0], hostAndPath[1])
+
+		if v, ok := hostsAndPaths[hostAndPath[0]]; ok {
+			for j := range v {
+				//fmt.Println("vj ", v[j], hostAndPath[1])
+				if v[j] == hostAndPath[1] {
+					flag = true
+					break
+				}
+			}
+
+			if flag == true {
+				continue
+			}else {
+				hostsAndPaths[hostAndPath[0]] = append(hostsAndPaths[hostAndPath[0]], hostAndPath[1])
+			}
+		}else {
+			hostsAndPaths[hostAndPath[0]] = append(hostsAndPaths[hostAndPath[0]], hostAndPath[1])
+		}
 	}
 
-	var host2 = Client{
-		userName: os.Args[1],
-		password: os.Args[2],
-		ipAddr: os.Args[5] + cSSHPort,
-		volume: os.Args[6],
+	//fmt.Println("show map keys and values.")
+	var req Client
+	req.userName = cUserName
+	req.password = pw
+
+	for k, v := range hostsAndPaths {
+		//fmt.Println("key is ", k)
+		req.ipAddr = k + cSSHPort
+		for p := range v {
+			//fmt.Println("path is ", v[p])
+			req.volume = v[p]
+
+			if err := req.deleteVolume(); err != nil {
+				fmt.Println(req.userName, req.password, req.ipAddr, req.volume, " failed to delete the specific volume.", err)
+				os.Exit(-1)
+			}
+		}
 	}
 
-	if err := host1.deleteVolume(); err != nil {
-		fmt.Println(host1.userName, host1.password, host1.ipAddr, host1.volume, " failed to delete the specific volume.", err)
-		os.Exit(-1)
-	}
-
-	if err := host2.deleteVolume(); err != nil {
-		fmt.Println(host2.userName, host2.password, host2.ipAddr, host2.volume, " failed to delete the specific volume.", err)
-		os.Exit(-1)
-	}
+	//var host1 = Client{
+	//	userName: os.Args[1],
+	//	password: os.Args[2],
+	//	ipAddr: os.Args[3] + cSSHPort,
+	//	volume: os.Args[4],
+	//}
+	//
+	//var host2 = Client{
+	//	userName: os.Args[1],
+	//	password: os.Args[2],
+	//	ipAddr: os.Args[5] + cSSHPort,
+	//	volume: os.Args[6],
+	//}
+	//
+	//if err := host1.deleteVolume(); err != nil {
+	//	fmt.Println(host1.userName, host1.password, host1.ipAddr, host1.volume, " failed to delete the specific volume.", err)
+	//	os.Exit(-1)
+	//}
+	//
+	//if err := host2.deleteVolume(); err != nil {
+	//	fmt.Println(host2.userName, host2.password, host2.ipAddr, host2.volume, " failed to delete the specific volume.", err)
+	//	os.Exit(-1)
+	//}
 }
 
 
